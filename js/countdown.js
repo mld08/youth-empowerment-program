8c66d0a0ba80823e7684394220916f97cfd1fdad
MLD <mldiop08@gmail.com>
2024-12-09 16:04:35 +0000

    First commit of the project

---
+++ js/countdown.js
@@ -0,0 +1,30 @@
+const targetDate = new Date('December 10, 2024 00:00:00').getTime();
+
+const countdown = setInterval(function() {
+
+    // Obtenez la date et l'heure actuelles
+    const now = new Date().getTime();
+
+    // Calculez la différence entre la date cible et la date actuelle
+    const distance = targetDate - now;
+
+    // Calculer les jours, heures, minutes et secondes restantes
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Afficher le décompte
+    //document.getElementById("countdown").innerHTML = 
+      //`Il reste ${days} jours, ${hours} heures, ${minutes} minutes et ${seconds} secondes.`;
+      document.getElementById("days").innerHTML = `${days}`;
+      document.getElementById("hours").innerHTML = `${hours}`;
+      document.getElementById("minutes").innerHTML = `${minutes}`;
+      document.getElementById("seconds").innerHTML = `${seconds}`;
+
+    // Si le décompte est terminé, afficher un message
+    if (distance < 0) {
+      clearInterval(countdown);
+      document.getElementById("countdown").innerHTML = "Le décompte est terminé !";
+    }
+  }, 1000);
